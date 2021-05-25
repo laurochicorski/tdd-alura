aba51ebfd5d23637c5c72ba519dc5ca2adcdccee
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-05-25 20:15:37 -0300

    Implementando uma funcionalidade com TDD

---
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,23 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReajusteServiceTest {
+
+    @Test
+    public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejado() {
+        ReajusteService reajusteService = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.00"));
+
+        reajusteService.concederReajuste(funcionario, Desempenho.A_DESEJAR);
+
+        assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+    }
+}
