3337450be14e23bb475bae099196d9a7ebb5584e
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-05-25 20:57:01 -0300

    Exception

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -7,15 +7,26 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class BonusServiceTest {
 
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Lauro", LocalDate.now(), new BigDecimal("25000")));
 
-        assertEquals(new BigDecimal("0.00"), bonus);
+//      1ª opção
+//        assertThrows(IllegalArgumentException.class
+//                , () -> service.calcularBonus(new Funcionario("Lauro", LocalDate.now(), new BigDecimal("25000"))));
+
+//        2ª opção
+        try{
+            service.calcularBonus(new Funcionario("Lauro", LocalDate.now(), new BigDecimal("25000")));
+            fail("Nao disparou exceção para funcionario com salario maior que 10.000,00");
+        }catch (IllegalArgumentException e) {
+            assertEquals("Funcionario com salario maior que 10.000,00 não pode receber bonus", e.getMessage());
+        }
+
     }
 
     @Test
